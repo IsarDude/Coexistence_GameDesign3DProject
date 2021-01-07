ec09547c49af4908d3a28e6411ed9ab46b8d2d82
Lukas Fritsch <Lukas.fritsch-post@web.de>
2021-01-07 23:12:11 +0100

    movement Graviation field Moving box

---
+++ GD3D_2020/Assets/GraviationField.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SpeedEvent: UnityEvent<float>
+{
+}
+
+[System.Serializable]
+public class LevitateEvent: UnityEvent<float>
+{
+}
+
+public class GraviationField : MonoBehaviour
+{
+    bool up;
+    public float slowSpeed = 0.5f;
+    public float fastSpeed = 2.0f;
+    public float height;
+    public SpeedEvent slowdown = new SpeedEvent();
+    public LevitateEvent levitate = new LevitateEvent();
+    // Start is called before the first frame update
+    void Start()
+    {
+        up = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (up)
+        {
+            levitate.Invoke(height);
+        } else
+        {
+            Debug.Log("slowing");
+            slowdown.Invoke(slowSpeed);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (up)
+        {
+            return;
+        }
+            slowdown.Invoke(fastSpeed);
+            Debug.Log("fast");
+        
+     
+    }
+}
--- GD3D_2020/Assets/movableSlave.cs
+++ GD3D_2020/Assets/movableSlave.cs
@@ -2,16 +2,14 @@
 using UnityEngine.Events;
 
 [System.Serializable]
-public class colissionEvent: UnityEvent<bool>
+
+public class movableSlave : MonoBehaviour
 {
-}
-public class Moveable : MonoBehaviour
-{
+    
     public CharacterController controller;
-    public GameObject otherObject;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    public float playerSpeed = 2.0f;
+    private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
@@ -19,19 +17,17 @@ public class Moveable : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     public Transform cam;
-    public colissionEvent colission = new colissionEvent();
-    private bool moveable;
-    private bool isFloating;
   
+
+    private bool stop;
     private void Start()
     {
-        moveable = true;
-        isFloating = false;
+      
+
     }
 
     void Update()
     {
-       
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -49,67 +45,36 @@ public class Moveable : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            if (moveable && !isFloating)
-            {
-                controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
-            }
-            
+            controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
 
         }
 
 
-        
+
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer && !isFloating)
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
-       
-            playerVelocity.y += gravityValue * Time.deltaTime;
-            controller.Move(playerVelocity * Time.deltaTime);
-        
-    }
 
-    public void Levitate(float height)
-    {
-        
-        playerVelocity.y += playerVelocity.y += Mathf.Sqrt(height * -3.0f * gravityValue);
+        playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-        
     }
 
-
-
-    public void ChangePlayerSpeed(float speed)
-    {
-        this.playerSpeed = speed;
-        Debug.Log(playerSpeed);
-    }
-/*
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if(hit.gameObject.tag != "ground")
+        if (hit.gameObject.tag != "ground")
         {
-            otherObject.transform.parent = this.transform;
-            colission.Invoke(false);
+          
             Debug.Log("hit");
         }
-       
+
     }
 
-    private void OnCollisionExit(Collision collision)
+    public void handObverCollider(ControllerColliderHit hit)
     {
-         otherObject.transform.parent = this.transform.parent;
-        colission.Invoke(true);
-        Debug.Log("exited");
-    }
-
-    public void OtherObjectHitCollider(bool isMovable)
-    {
-        moveable = isMovable;
+        controller.SendMessage("OnControllerColliderHit", hit);
         Debug.Log("handedTochonroller");
     }
-*/
 }
