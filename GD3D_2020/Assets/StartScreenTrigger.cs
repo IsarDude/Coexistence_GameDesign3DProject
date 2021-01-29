123249b99785fa09b4ffe2cee102504c28e97f60
Lukas <lukas.fritsch@hof-university.de>
2021-01-29 21:40:54 +0100

    alterd start point

---
+++ GD3D_2020/Assets/StartScreenTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenTrigger : MonoBehaviour
+{
+    public StartScreen startScreen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        startScreen.Pause();
+        this.gameObject.SetActive(false);
+    }
+}
