42d950efa9ff3f4e7e01297e3fd01ff3cac50029
Lukas <lukas.fritsch@hof-university.de>
2021-01-29 21:13:16 +0100

    added end door and StartScreen

---
+++ GD3D_2020/Assets/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    public GameObject startScreen;
+    public bool GameIsPaused = false;
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        startScreen.SetActive(false);
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        startScreen.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
